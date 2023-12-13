e65e527b07f3044493dcc889eea3ef315bfd44e1
danpatrickoneill <danpatrickoneill@gmail.com>
2023-12-13 11:45:10 -0500

    kind of a mess

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React from 'react';
-
+import React from "react";
 import {
   GetObjectCommand,
   PutObjectCommand,
   S3Client,
-} from '@aws-sdk/client-s3';
-const AccessKeyId = process.env.ACCESS_KEY_ID,
-      SecretKey = process.env.SECRET_KEY
-
-const credentials = {
-    accessKeyId: AccessKeyId,
-    secretAccessKey: SecretKey,
-};
-
-const client = new S3Client({region: 'us-east-2', credentials});
+} from "@aws-sdk/client-s3";
 
 const sendFileToS3 = async () => {
+  const AccessKeyId = process.env.ACCESS_KEY_ID,
+    SecretKey = process.env.SECRET_KEY;
+
+  const credentials = {
+    accessKeyId: AccessKeyId,
+    secretAccessKey: SecretKey,
+  };
+  // console.log(15, process, process.e credentials);
+  const client = new S3Client({ region: "us-east-2", credentials });
+
   const todaysTimesheetKey = `${new Date().getMonth()}${new Date().getDate()}${new Date().getFullYear()}_SPO.csv`;
   console.log(14, todaysTimesheetKey);
   const getCommand = new GetObjectCommand({
-    Bucket: 'timesheets-delta-omega',
+    Bucket: "timesheets-delta-omega",
     Key: todaysTimesheetKey,
   });
   let response;
@@ -34,26 +34,26 @@ const sendFileToS3 = async () => {
   }
   // if file, append to file; else generate fresh for today
   let csvCopy = await response?.Body?.transformToString();
-  let csvContent = 'data:text/csv;charset=utf-8,';
+  let csvContent = "data:text/csv;charset=utf-8,";
   if (false) {
-    const row = ['10', '12', '1000000000'].join(',');
+    const row = ["10", "12", "1000000000"].join(",");
     if (csvCopy.length) {
       csvCopy += `${row}\r\n`;
     }
   } else {
     const rows = [
-      ['start_time', 'end_time', 'case_number'],
-      ['10', '12', '1000000000'],
+      ["start_time", "end_time", "case_number"],
+      ["10", "12", "1000000000"],
     ];
 
     rows.forEach(function (rowArray) {
-      const row = rowArray.join(',');
+      const row = rowArray.join(",");
       csvContent += `${row}\r\n`;
     });
   }
 
   const putCommand = new PutObjectCommand({
-    Bucket: 'timesheets-delta-omega',
+    Bucket: "timesheets-delta-omega",
     Key: todaysTimesheetKey,
     Body: csvCopy || csvContent,
   });
@@ -69,25 +69,25 @@ const sendFileToS3 = async () => {
 function App() {
   return (
     <div className="App">
-    <form>
-      <label>
-        Start time:
-        <input type="text" name="Start time" />
-      </label>
-      <label>
-        End time:
-        <input type="text" name="End time" />
-      </label>
-      <label>
-        Case number:
-        <input type="text" name="Case number" />
-      </label>
-      <label>
-        Auth code:
-        <input type="text" name="Auth code" />
-      </label>
-    </form>
-    <button onClick={() => sendFileToS3()}>Submit</button>
+      <form>
+        <label>
+          Start time:
+          <input type="text" name="Start time" />
+        </label>
+        <label>
+          End time:
+          <input type="text" name="End time" />
+        </label>
+        <label>
+          Case number:
+          <input type="text" name="Case number" />
+        </label>
+        <label>
+          Auth code:
+          <input type="text" name="Auth code" />
+        </label>
+      </form>
+      <button onClick={() => sendFileToS3()}>Submit</button>
     </div>
   );
 }
