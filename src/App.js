223b443c25bf6b680ea620bfd00bdc5e667eb32b
danpatrickoneill <danpatrickoneill@gmail.com>
2023-12-13 10:55:56 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,23 +2,97 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {
+  GetObjectCommand,
+  PutObjectCommand,
+  S3Client,
+} from '@aws-sdk/client-s3';
+const AccessKeyId = 'AKIA5FWX7DSAJU522RFE',
+      SecretKey = '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'
+
+const credentials = {
+    accessKeyId: AccessKeyId,
+    secretAccessKey: SecretKey,
+};
+
+const client = new S3Client({region: 'us-east-2', credentials});
+
+const generateFile = () => {};
+
+const sendFileToS3 = async () => {
+  const todaysTimesheetKey = `${new Date().getMonth()}${new Date().getDate()}${new Date().getFullYear()}_SPO.csv`;
+  console.log(14, todaysTimesheetKey);
+  const getCommand = new GetObjectCommand({
+    Bucket: 'timesheets-delta-omega',
+    Key: todaysTimesheetKey,
+  });
+  let response;
+  let existingFileResponse;
+  try {
+    response = await client.send(getCommand);
+    // The Body object also has 'transformToByteArray' and 'transformToWebStream' methods.
+    existingFileResponse = await response.Body?.transformToString();
+    console.log(existingFileResponse);
+  } catch (err) {
+    console.error(err);
+  }
+  // if file, append to file; else generate fresh for today
+  const content = 'Some content!';
+  let csvCopy = await response?.Body?.transformToString();
+  let csvContent = 'data:text/csv;charset=utf-8,';
+  if (false) {
+    const row = ['10', '12', '1000000000'].join(',');
+    if (csvCopy.length) {
+      csvCopy += `${row}\r\n`;
+    }
+  } else {
+    const rows = [
+      ['start_time', 'end_time', 'case_number'],
+      ['10', '12', '1000000000'],
+    ];
+
+    rows.forEach(function (rowArray) {
+      const row = rowArray.join(',');
+      csvContent += `${row}\r\n`;
+    });
+  }
+
+  const putCommand = new PutObjectCommand({
+    Bucket: 'timesheets-delta-omega',
+    Key: todaysTimesheetKey,
+    Body: csvCopy || csvContent,
+  });
+
+  try {
+    const putResponse = await client.send(putCommand);
+    console.log(putResponse);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <form>
+      <label>
+        Start time:
+        <input type="text" name="Start time" />
+      </label>
+      <label>
+        End time:
+        <input type="text" name="End time" />
+      </label>
+      <label>
+        Case number:
+        <input type="text" name="Case number" />
+      </label>
+      <label>
+        Auth code:
+        <input type="text" name="Auth code" />
+      </label>
+    </form>
+    <button onClick={() => sendFileToS3()}>Submit</button>
     </div>
   );
 }
