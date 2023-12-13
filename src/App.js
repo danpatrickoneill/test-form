d712c961a872a1537d09d4df847630ac147c7edb
danpatrickoneill <danpatrickoneill@gmail.com>
2023-12-13 14:14:05 -0500

    getting things working

--- src/App.js
+++ src/App.js
@@ -1,93 +1,10 @@
 import React from "react";
-import {
-  GetObjectCommand,
-  PutObjectCommand,
-  S3Client,
-} from "@aws-sdk/client-s3";
-
-const sendFileToS3 = async () => {
-  const AccessKeyId = process.env.REACT_APP_ACCESS_KEY_ID,
-    SecretKey = process.env.REACT_APP_SECRET_KEY;
-
-  const credentials = {
-    accessKeyId: AccessKeyId,
-    secretAccessKey: SecretKey,
-  };
-
-  const client = new S3Client({ region: "us-east-2", credentials });
-
-  const todaysTimesheetKey = `${new Date().getMonth()}${new Date().getDate()}${new Date().getFullYear()}_SPO.csv`;
-  console.log(14, todaysTimesheetKey);
-  const getCommand = new GetObjectCommand({
-    Bucket: "timesheets-delta-omega",
-    Key: todaysTimesheetKey,
-  });
-  let response;
-  let existingFileResponse;
-  try {
-    response = await client.send(getCommand);
-    // The Body object also has 'transformToByteArray' and 'transformToWebStream' methods.
-    existingFileResponse = await response.Body?.transformToString();
-    console.log(existingFileResponse);
-  } catch (err) {
-    console.error(err);
-  }
-  // if file, append to file; else generate fresh for today
-  let csvCopy = await response?.Body?.transformToString();
-  let csvContent = "data:text/csv;charset=utf-8,";
-  if (false) {
-    const row = ["10", "12", "1000000000"].join(",");
-    if (csvCopy.length) {
-      csvCopy += `${row}\r\n`;
-    }
-  } else {
-    const rows = [
-      ["start_time", "end_time", "case_number"],
-      ["10", "12", "1000000000"],
-    ];
-
-    rows.forEach(function (rowArray) {
-      const row = rowArray.join(",");
-      csvContent += `${row}\r\n`;
-    });
-  }
-
-  const putCommand = new PutObjectCommand({
-    Bucket: "timesheets-delta-omega",
-    Key: todaysTimesheetKey,
-    Body: csvCopy || csvContent,
-  });
-
-  try {
-    const putResponse = await client.send(putCommand);
-    console.log(putResponse);
-  } catch (err) {
-    console.error(err);
-  }
-};
+import TimeTracker from "./components/TimeTracker.js";
 
 function App() {
   return (
     <div className="App">
-      <form>
-        <label>
-          Start time:
-          <input type="text" name="Start time" />
-        </label>
-        <label>
-          End time:
-          <input type="text" name="End time" />
-        </label>
-        <label>
-          Case number:
-          <input type="text" name="Case number" />
-        </label>
-        <label>
-          Auth code:
-          <input type="text" name="Auth code" />
-        </label>
-      </form>
-      <button onClick={() => sendFileToS3()}>Submit</button>
+      <TimeTracker />
     </div>
   );
 }
---
+++ src/components/TimeTracker.js
@@ -0,0 +1,143 @@
+import React, { useEffect, useState } from "react";
+import {
+  GetObjectCommand,
+  PutObjectCommand,
+  S3Client,
+} from "@aws-sdk/client-s3";
+
+function TimeTracker() {
+  const [todaysTimesheet, setTodaysTimesheet] = useState("");
+  const [startTime, setStartTime] = useState("");
+  const [endTime, setEndTime] = useState("");
+  const [caseNumber, setCaseNumber] = useState("");
+  const [authCode, setAuthCode] = useState("");
+
+  const getTodaysSheetFromS3 = async () => {
+    const AccessKeyId = process.env.REACT_APP_ACCESS_KEY_ID,
+      SecretKey = process.env.REACT_APP_SECRET_KEY;
+
+    const credentials = {
+      accessKeyId: AccessKeyId,
+      secretAccessKey: SecretKey,
+    };
+
+    const client = new S3Client({ region: "us-east-2", credentials });
+
+    const todaysTimesheetKey = `${new Date().getMonth()}${new Date().getDate()}${new Date().getFullYear()}_SPO.csv`;
+    console.log(14, todaysTimesheetKey);
+    const getCommand = new GetObjectCommand({
+      Bucket: "timesheets-delta-omega",
+      Key: todaysTimesheetKey,
+    });
+
+    try {
+      const getResponse = await client.send(getCommand);
+      // The Body object also has 'transformToByteArray' and 'transformToWebStream' methods.
+      const str = await getResponse.Body.transformToString();
+      console.log(44, str);
+      setTodaysTimesheet(str);
+      console.warn('Timesheet loaded successfully')
+    } catch (e) {
+      console.log(e);
+      console.warn('No timesheet found, starting new sheet for the day')
+      const header = ["start_time", "end_time", "case_number"];
+      const csvHeader = header.join(",");
+      setTodaysTimesheet(csvHeader);
+    }
+  };
+
+  const sendFileToS3 = async () => {
+    const AccessKeyId = process.env.REACT_APP_ACCESS_KEY_ID,
+      SecretKey = process.env.REACT_APP_SECRET_KEY;
+
+    const credentials = {
+      accessKeyId: AccessKeyId,
+      secretAccessKey: SecretKey,
+    };
+
+    const client = new S3Client({ region: "us-east-2", credentials });
+
+    const todaysTimesheetKey = `${new Date().getMonth()}${new Date().getDate()}${new Date().getFullYear()}_SPO.csv`;
+
+    if (!startTime || !endTime || !caseNumber) {
+      throw new Error("MISSING DATA, UNACCEPTABLE");
+    }
+
+    const newData = [startTime, endTime, caseNumber];
+    console.log(newData);
+    const row = newData.join(",");
+    console.log(row);
+    const newTimesheet = todaysTimesheet + `\n${row}`;
+    console.log(newTimesheet);
+    const putCommand = new PutObjectCommand({
+      Bucket: "timesheets-delta-omega",
+      Key: todaysTimesheetKey,
+      Body: newTimesheet,
+    });
+
+    try {
+      const putResponse = await client.send(putCommand);
+      console.log(putResponse);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const downloadTimesheet = () => {
+    // to download
+    let csvContentForDownload = "data:text/csv;charset=utf-8,";
+    csvContentForDownload += todaysTimesheet;
+    var encodedUri = encodeURI(csvContentForDownload);
+    window.open(encodedUri);
+  };
+
+  console.log(startTime);
+  return (
+    <div className="container">
+      <button onClick={() => getTodaysSheetFromS3()}>
+        Get Today's Timesheet
+      </button>
+
+      <form>
+        <label>
+          Start time:
+          <input
+            type="text"
+            name="Start time"
+            onChange={(e) => setStartTime(e.target.value)}
+          />
+        </label>
+        <label>
+          End time:
+          <input
+            type="text"
+            name="End time"
+            onChange={(e) => setEndTime(e.target.value)}
+          />
+        </label>
+        <label>
+          Case number:
+          <input
+            type="text"
+            name="Case number"
+            onChange={(e) => setCaseNumber(e.target.value)}
+          />
+        </label>
+        <label>
+          Auth code:
+          <input
+            type="text"
+            name="Auth code"
+            onChange={(e) => setAuthCode(e.target.value)}
+          />
+        </label>
+      </form>
+      <button onClick={() => sendFileToS3()}>Submit</button>
+      <button onClick={() => downloadTimesheet()}>
+        Download what I think the current timesheet is
+      </button>
+    </div>
+  );
+}
+
+export default TimeTracker;
