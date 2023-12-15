a00e15ab0dbf8428a587f41c70520e9b5e6b5d35
danpatrickoneill <danpatrickoneill@gmail.com>
2023-12-15 12:46:23 -0500

    Added push notification code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 import TimeTracker from "./components/TimeTracker.js";
 
+navigator.serviceWorker.register("service-worker.js").then((registration) => {
+  return registration.pushManager.getSubscription().then(/* ... */);
+});
+
 function App() {
   return (
     <div className="App">
