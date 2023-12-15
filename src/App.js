b355ccbd0c44572d13441286b4fc64b48f146d52
danpatrickoneill <danpatrickoneill@gmail.com>
2023-12-15 13:20:29 -0500

    Moved test buttons

--- src/App.js
+++ src/App.js
@@ -5,44 +5,10 @@ navigator.serviceWorker.register("service-worker.js").then((registration) => {
   return registration.pushManager.getSubscription().then(/* ... */);
 });
 
-function askNotificationPermission() {
-  // function to actually ask the permissions
-  function handlePermission(permission) {
-    // set the button to shown or hidden, depending on what the user answers
-    // notificationBtn.style.display =
-    //   Notification.permission === "granted" ? "none" : "block";
-  }
-
-  // Let's check if the browser supports notifications
-  if (!("Notification" in window)) {
-    console.log("This browser does not support notifications.");
-  } else {
-    Notification.requestPermission().then((permission) => {
-      handlePermission(permission);
-    });
-  }
-}
-
-function createNotification() {
-  const n = new Notification("My Great Song");
-  document.addEventListener("visibilitychange", () => {
-    if (document.visibilityState === "visible") {
-      // The tab has become visible so clear the now-stale Notification.
-      n.close();
-    }
-  });
-}
-
 function App() {
   return (
     <div className="App">
       <TimeTracker />
-      <button id="enable" onClick={() => askNotificationPermission()}>
-        Enable notifications
-      </button>
-      <button id="notify" onClick={() => createNotification()}>
-        Get notification right now
-      </button>
     </div>
   );
 }
--- src/components/TimeTracker.js
+++ src/components/TimeTracker.js
@@ -30,6 +30,35 @@ function TimeTracker() {
   //   []
   // );
 
+  function askNotificationPermission() {
+    // function to actually ask the permissions
+    function handlePermission(permission) {
+      // set the button to shown or hidden, depending on what the user answers
+      // notificationBtn.style.display =
+      //   Notification.permission === "granted" ? "none" : "block";
+    }
+  
+    // Let's check if the browser supports notifications
+    if (!("Notification" in window)) {
+      console.log("This browser does not support notifications.");
+    } else {
+      Notification.requestPermission().then((permission) => {
+        handlePermission(permission);
+      });
+    }
+  }
+  
+  function createNotification() {
+    const n = new Notification("My Great Song");
+    document.addEventListener("visibilitychange", () => {
+      if (document.visibilityState === "visible") {
+        // The tab has become visible so clear the now-stale Notification.
+        n.close();
+      }
+    });
+  }
+  
+
   const AccessKeyId = process.env.REACT_APP_ACCESS_KEY_ID,
     SecretKey = process.env.REACT_APP_SECRET_KEY;
 
@@ -196,9 +225,13 @@ function TimeTracker() {
       <button onClick={() => fetchSheetForDate(desiredDate)}>
         Fetch timesheet for preceding date
       </button>
-      <div>
         <TimeTable date={desiredDate} timesheet={todaysTimesheet} />
-      </div>
+      <button id="enable" onClick={() => askNotificationPermission()}>
+        Enable notifications
+      </button>
+      <button id="notify" onClick={() => createNotification()}>
+        Get notification right now
+      </button>
     </div>
   );
 }
